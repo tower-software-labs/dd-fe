25b67cbf23717a77f732f963d80e6f57c8adf342
Adam Dorfman <github.rgz6q@slmail.me>
2024-10-14 15:50:22 -0700

    [WIP] tasks page

--- src/app/task/[slug]/page.tsx
+++ src/app/task/[slug]/page.tsx
@@ -8,13 +8,21 @@ import {
   RequestListTableHeader,
   RequestListTaskRow,
 } from "@/components/request-list-table"
+import SelectUserPopover from "@/components/select-user-popover"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Table, TableBody } from "@/components/ui/table"
 import { Textarea } from "@/components/ui/textarea"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 import { useBreadcrumbs } from "@/providers/breadcrumb-provider"
 import { Task, TaskState } from "@/types/task"
-import { FolderClosed } from "lucide-react"
+import { BellIcon, FolderClosed, Pencil, Save } from "lucide-react"
 import { useEffect, useState } from "react"
 
 export interface TaskPageProps {
@@ -29,6 +37,8 @@ export default function TaskPage({ params }: TaskPageProps) {
   const [task, setTask] = useState<Task | null>(null)
   const [subtasks, setSubtasks] = useState<Task[]>([])
 
+  const [isEditing, setIsEditing] = useState(false)
+
   function updateTask(field: keyof Task, value: any) {
     setTask((prevTask) => (prevTask ? { ...prevTask, [field]: value } : null))
   }
@@ -39,6 +49,10 @@ export default function TaskPage({ params }: TaskPageProps) {
         subtask.id === taskId ? { ...subtask, [field]: value } : subtask,
       ),
     )
+  }
+
+  function handleSave() {
+    setIsEditing(false)
   }
 
   function addSubtask({ description, title, assignee }: addNewTaskProps) {
@@ -75,14 +89,79 @@ export default function TaskPage({ params }: TaskPageProps) {
       <div className="space-y-8 border border-gray-200 p-6 rounded-lg">
         <div className="space-y-2 mb-6">
           <div className="flex justify-between items-center">
-            <h1 className="text-2xl font-bold">
-              {task?.title ? `${task.id} ${task?.title}` : "Task"}
-            </h1>
+            <div className="flex flex-row items-center text-2xl font-bold w-full">
+              <span className="font-mono mr-2">{task?.id}</span>
+              {isEditing ? (
+                <div className="flex-grow max-w-lg">
+                  <Input
+                    value={task?.title ?? ""}
+                    onChange={(e) => updateTask("title", e.target.value)}
+                    className="text-2xl font-bold max-w-lg"
+                  />
+                </div>
+              ) : (
+                <h1 className="text-2xl font-bold">
+                  <span>{task?.title ? task?.title : "Task"}</span>
+                </h1>
+              )}
+              <div className="mt-2 ml-4">
+                <SelectUserPopover
+                  selectedUserId={task?.assignee?.id ?? null}
+                  setSelectedUserId={(value: string | null) =>
+                    updateTask("assignee", value)
+                  }
+                />
+              </div>
+            </div>
+            <div className="flex space-x-2 w-fit">
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                      <BellIcon className="h-4 w-4" />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Send Reminder</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      onClick={() =>
+                        isEditing ? handleSave() : setIsEditing(true)
+                      }
+                    >
+                      {isEditing ? (
+                        <Save className="h-4 w-4" />
+                      ) : (
+                        <Pencil className="h-4 w-4" />
+                      )}
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{isEditing ? "Save" : "Edit"}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
           </div>
           <div className="space-y-2">
-            <span className="text-sm text-slate-500">
-              {task?.description ?? "Description"}
-            </span>
+            {isEditing ? (
+              <Textarea
+                value={task?.description ?? ""}
+                onChange={(e) => updateTask("description", e.target.value)}
+                className="text-sm text-slate-500 max-w-[80svh] min-h-[100px]"
+              />
+            ) : (
+              <span className="text-sm text-slate-500">
+                {task?.description ?? "Description"}
+              </span>
+            )}
           </div>
         </div>
         <div className="mb-6">
@@ -90,6 +169,7 @@ export default function TaskPage({ params }: TaskPageProps) {
             value={task?.state ?? undefined}
             onValueChange={(value) => updateTask("state", value as TaskState)}
             className="flex items-center space-x-6"
+            disabled={!isEditing}
           >
             {["Not Applicable", "To Be Provided", "Provided"].map((status) => (
               <div key={status} className="flex items-center">
@@ -107,10 +187,17 @@ export default function TaskPage({ params }: TaskPageProps) {
         </div>
         <div>
           <h2 className="text-lg font-semibold mb-2">Sellside Comments</h2>
-          <Textarea
-            value={task?.sellsideComments ?? ""}
-            onChange={(e) => updateTask("sellsideComments", e.target.value)}
-          />
+          {isEditing ? (
+            <Textarea
+              value={task?.sellsideComments ?? ""}
+              onChange={(e) => updateTask("sellsideComments", e.target.value)}
+              className="w-full max-w-[80svh] min-h-[100px]"
+            />
+          ) : (
+            <div className="border border-gray-200 rounded-md p-3 min-h-[100px] max-w-[80svh]">
+              {task?.sellsideComments || "No comments"}
+            </div>
+          )}
         </div>
       </div>
       <div className="space-y-2 mt-4">
